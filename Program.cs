d0f7ab4e8efca27854a3992c2b74799d84eb9c7d
jgrissom <jgrissom91@gmail.com>
2024-10-22 14:39:51 -0500

    [2.01] Create user options Display all blogs Add Blog

--- Program.cs
+++ Program.cs
@@ -6,24 +6,43 @@ var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassL
 
 logger.Info("Program started");
 
-
-// Create and save a new Blog
-Console.Write("Enter a name for a new Blog: ");
-var name = Console.ReadLine();
-
-var blog = new Blog { Name = name };
-
-var db = new DataContext();
-db.AddBlog(blog);
-logger.Info("Blog added - {name}", name);
-
-// Display all Blogs from the database
-var query = db.Blogs.OrderBy(b => b.Name);
-
-Console.WriteLine("All blogs in the database:");
-foreach (var item in query)
+do
 {
-  Console.WriteLine(item.Name);
-}
+  Console.WriteLine("Enter your selection:");
+  Console.WriteLine("1) Display all blogs");
+  Console.WriteLine("2) Add Blog");
+  Console.WriteLine("Enter to quit");
+  string? choice = Console.ReadLine();
+  Console.Clear();
+  logger.Info("Option {choice} selected", choice);
+
+  if (choice == "1")
+  {
+    // display blogs
+    var db = new DataContext();
+    var query = db.Blogs.OrderBy(b => b.Name);
+
+    Console.WriteLine($"{query.Count()} Blogs returned");
+    foreach (var item in query)
+    {
+      Console.WriteLine(item.Name);
+    }
+  }
+  else if (choice == "2")
+  {
+    // Add blog
+    Console.Write("Enter a name for a new Blog: ");
+    var blog = new Blog { Name = Console.ReadLine() };
+
+    var db = new DataContext();
+    db.AddBlog(blog);
+    logger.Info("Blog added - {name}", blog.Name);
+  }
+  else if (String.IsNullOrEmpty(choice))
+  {
+    break;
+  }
+  Console.WriteLine();
+} while (true);
 
 logger.Info("Program ended");
